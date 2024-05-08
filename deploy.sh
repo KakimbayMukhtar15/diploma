760b10a015c4cfc614d1e96b9291ab0dde2fcd67
KakimbayMukhtar15 <120240391+KakimbayMukhtar15@users.noreply.github.com>
2024-05-09 01:49:42 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,9 @@ sudo mv  * /var/www/diploma
 cd /var/www/diploma/
 sudo mv env .env
 
+sudo python3 -m venv venv
+source venv/bin/activate
+
 sudo apt-get update
 echo "installing python and pip"
 sudo apt-get install -y python3 python3-pip
